22b56932040d73cb2a6896a859a2d5ba43ae623c
QosimovQuvonch <qosimovquvonc@gmail.com>
2025-11-25 09:43:55 +0500

    Asosiy operatorlar

---
+++ Asosiy-operatorlar.js
@@ -0,0 +1,47 @@
+let a = 5,
+  b = 10,
+  c = "5";
+const e = a;
+console.log(a + b); 
+console.log(a - b); 
+console.log(a * b); 
+console.log(a / b); 
+console.log(a++);
+console.log(a);
+console.log(++a);
+console.log(a);
+console.log(b--);
+console.log(b);
+console.log(--b);
+console.log(b);
+console.log(a % b);
+console.log(a == c);
+console.log(a === c);
+console.log(a == e);
+console.log(a === e);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a != b);
+console.log(a += b);
+console.log(a -= b);
+console.log(a *= b);
+console.log(a /= b);
+console.log(a = b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
