ddfbab5e2b1e5be14718e3fe93ddbe4df2ec64b9
QosimovQuvonch <qosimovquvonc@gmail.com>
2025-11-27 10:44:06 +0500

    Function

---
+++ Javascript/funktion.js
@@ -0,0 +1,13 @@
+function kvadrat(a) {
+    return a * a;
+}
+console.log(kvadrat(2));
+
+const kvadrat1 = function(b){
+    return b * b 
+}
+console.log(kvadrat1(4));
+
+const kvadrat2 = (c) => c * c;
+console.log(kvadrat2(6));
+
