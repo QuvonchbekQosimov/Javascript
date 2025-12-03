18d8ca4a33767b3ea3e442aff822e4e9bd0faebc
QosimovQuvonch <qosimovquvonc@gmail.com>
2025-12-03 11:51:32 +0500

    Objects

---
+++ Javascript/objects.js
@@ -0,0 +1,15 @@
+const talaba = {
+    ismi: 'Quvonchbek',
+    familiyasi: 'Qosimov',
+    yoshi: 19,
+    manzil: {
+        viloyat: 'Surxondaryo',
+        tuman: 'Qumqo`rg`on',
+        mahalla: 'Istiqlol'
+    }
+}
+console.log(talaba);
+console.log(talaba.ismi);
+console.log(talaba.manzil.tuman);
+
+
