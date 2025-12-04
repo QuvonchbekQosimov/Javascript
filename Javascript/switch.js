c46c2f4ce6d3721eebeee8e312a4d5594cc6839f
QosimovQuvonch <qosimovquvonc@gmail.com>
2025-12-04 16:49:42 +0500

    switch

---
+++ Javascript/switch.js
@@ -0,0 +1,21 @@
+const number = 4;
+let natija;
+switch (number) {
+  case 1:
+    natija = "bir";
+    break;
+  case 2:
+    natija = "ikki";
+    break;
+  case 3:
+    natija = "uch";
+    break;
+  case 4:
+    natija = "to`rt";
+    break;
+
+  default:
+    natija = "topilmadi";
+    break;
+}
+console.log(natija);
