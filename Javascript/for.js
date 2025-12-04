82211740b8287e101329512db6e539ce9901b613
QosimovQuvonch <qosimovquvonc@gmail.com>
2025-12-04 17:15:38 +0500

    For

---
+++ Javascript/for.js
@@ -0,0 +1,10 @@
+let a = prompt("Sonni kiriting :")
+for (let i = 0; i <= a; i++) {
+   console.log(i);
+}
+
+let b = prompt("Sonni kiriting :")
+let natija = 0;
+for (let i = 0; i <= b; i++) {
+    natija += i;
+}
