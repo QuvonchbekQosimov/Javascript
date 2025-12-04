b88533a4ab12f413bee6aad39340b4c836f13006
QosimovQuvonch <qosimovquvonc@gmail.com>
2025-12-04 09:59:50 +0500

    if / else - if / else

---
+++ Javascript/if-else.js
@@ -0,0 +1,31 @@
+//if
+const number1 = 1;
+if (number1 > 0) {
+  console.log(`${number1} musbat son`);
+}
+console.log("if dan keyingi qator");
+
+//else
+const number2 = -1;
+if (number2 > 0) {
+  console.log(`${number2} musbat son`);
+}
+console.log("if dan keyingi qator");
+
+//if-else
+const number3 = 0;
+if (number3 > 5) {
+  console.log(`${number3} 5 dan katta`);
+} else {
+  console.log(`${number3} 5 dan kichik`);
+}
+
+//if/else-if/else
+const number4 = 0;
+if (number4 > 0) {
+  console.log("Musbat");
+} else if (number4 == 0) {
+  console.log(0);
+} else {
+  console.log("manfiy");
+}
