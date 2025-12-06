deb17158ff11c971f77439caa9195be752b7c8d7
QosimovQuvonch <qosimovquvonc@gmail.com>
2025-12-06 11:22:32 +0500

    DOM1

---
+++ Javascript/dom.js
@@ -0,0 +1,11 @@
+const idName = document.getElementById('id');
+idName.remove();
+
+const className = document.getElementsByClassName('class');
+className.remove();
+
+const nameName = document.getElementsByName('name');
+nameName.remove();
+
+const tagName = document.getElementsByTagName('p');
+tagName.remove();
